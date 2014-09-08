9e828df30ba81ef3471fd51159133e5182668ebf
Dennis Gokman <dennis.gokman@gmail.com>
2014-09-08 14:12:51 -0400

    added pagination

--- app.rb
+++ app.rb
@@ -7,7 +7,14 @@ require_relative 'models/contact'
 
 
 get '/' do
-  @contacts = Contact.all
+  # @contacts = Contact.all
+  if params[:page] == nil
+    @contacts = Contact.limit(3)
+    @next_page = 2
+  else
+    @contacts = Contact.limit(3).offset(3 * (params[:page].to_i - 1))
+    @next_page = params[:page].to_i + 1
+  end
   erb :index
 end
 
