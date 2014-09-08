50917ce4da274dcbdee9c448bd3193c32570da8b
Dennis Gokman <dennis.gokman@gmail.com>
2014-09-08 16:41:10 -0400

    search functionality working

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ get '/' do
   @contacts_all = Contact.all
   @limit = 3
   if params[:query] && params[:page].nil?
-    @contacts = Contact.where("first_name = ? OR last_name = ?", params[:query], params[:query])
+    @contacts = Contact.where("first_name = ? OR last_name = ?", params[:query].capitalize, params[:query].capitalize)
   elsif params[:query].nil? && params[:page].nil?
     @contacts = Contact.limit(@limit)
     @next_page = 2
