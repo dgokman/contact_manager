cfd4f98e88503862bdbcadc2cabf6f2143563de1
Dennis Gokman <dennis.gokman@gmail.com>
2014-09-08 15:57:39 -0400

    added search form, functionality in progress

--- app.rb
+++ app.rb
@@ -9,7 +9,10 @@ require_relative 'models/contact'
 get '/' do
   @contacts_all = Contact.all
   @limit = 3
-  if params[:page] == nil
+  if params[:query] && params[:page].nil?
+    @contacts = Contact.where(first_name: params[:query])
+
+  elsif params[:query].nil? && params[:page].nil?
     @contacts = Contact.limit(@limit)
     @next_page = 2
   else
@@ -18,11 +21,13 @@ get '/' do
     @prev_page = params[:page].to_i - 1
   end
   erb :index
+  #binding.pry
 end
 
 get '/contacts/:id' do
   @contact = Contact.find(params[:id])
   erb :show
 end
+
 
 
